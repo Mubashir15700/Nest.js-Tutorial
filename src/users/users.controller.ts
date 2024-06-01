99900b36de95f09ad52e38ecda3c6806f75ce13c
Mubashir15700 <mubashir15700@gmail.com>
2024-06-01 19:58:26 +0530

    learn controllers, path and query params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // same as /users
+export class UsersController {
+    @Get() // "GET /users" with optional single query param
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return [{ role }];
+    }
+
+    @Get('filtered') // "GET /users/filtered?role=admin&name=niko" with option role and required name query params
+    findAllWithName(@Query() query:
+        { role?: 'INTERN' | 'ENGINEER' | 'ADMIN', name: string }
+    ) {
+        const { role, name } = query;
+        return [{ role, name }];
+    }
+
+    @Get('interns') // GET /users/interns
+    findAllInterns() {
+        return [];
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id };
+    }
+
+    @Post() // POST /users
+    create(@Body() user: {}) {
+        console.log(user);
+        return user;
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    findOneAndUpdate(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate };
+    }
+
+    @Delete(':id') // DELETE /users/:id
+    findOneAndDelete(@Param('id') id: string) {
+        return { id };
+    }
+}
